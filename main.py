dcc55b7be8dccbc1ca365c3d4d75e7fe2258b595
malcolmling97 <malcolmling97@gmail.com>
2025-01-20 14:05:02 +0800

    completed bookbot

---
+++ main.py
@@ -0,0 +1,42 @@
+def main():
+    path = "books/frankenstein.txt"
+    frank_contents = get_contents(path)
+    frank_words = get_count_words(frank_contents)
+    frank_char_dict = get_char_count(frank_contents)
+
+    # print (frank_contents)
+    print ("--- Begin report of books/frankenstein.txt ---")
+    print (f"{frank_words} words were found in the document")
+    # print (sorted(frank_char_dict.items(),key=lambda x: x[1], reverse=True))
+    sorted_list = sorted(frank_char_dict.items(), key=lambda x: x[1], reverse=True)
+    for i in range(0,len(sorted_list)):
+        print(f"The '{sorted_list[i][0]}' character was found {sorted_list[i][1]} times")
+    
+    print("--- End report ---")
+
+
+def get_contents(path):
+    with open(path) as f:
+        return f.read()
+
+def get_count_words(content):
+    words = content.split()
+    return len(words)
+
+def get_char_count(content):
+    dict = {}
+    lowered_content = content.lower()
+    for letter in lowered_content:
+        if letter.isalpha():
+            if letter not in dict:
+                dict[letter] = 1
+            else:
+                dict[letter] += 1
+ 
+    return dict
+
+
+
+
+
+main()
